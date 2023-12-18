a9197b306a0f50a37a9ddc0d0c92974052316299
DashaKhilko <zhukdo@mail.ru>
2023-12-18 14:18:18 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-Bag-Store",
+  //base: "/vue-Bag-Store",
   plugins: [
     vue(),
     vueJsx(),
