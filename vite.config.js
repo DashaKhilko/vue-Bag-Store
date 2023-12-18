b293ce0b7fda0e26547db2f5dfeb987f1e8cee4f
DashaKhilko <zhukdo@mail.ru>
2023-12-18 13:56:18 +0300

    remove dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/vue-Bag-Store`,
+  publicPath: `/vue-Bag-Store/`,
   plugins: [
     vue(),
     vueJsx(),
